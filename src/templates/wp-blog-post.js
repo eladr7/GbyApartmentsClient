21a9ee0234fc269a3358bb21d9a6cc6bf2df4646
elad <elad@twik.io>
2020-07-27 19:59:01 +0300

    Styled the apartment page

--- src/templates/wp-blog-post.js
+++ src/templates/wp-blog-post.js
@@ -4,22 +4,27 @@ import Layout from "../components/layout"
 import Img from "gatsby-image"
 
 export default function BlogPost({ data }) {
-    const post = data.allWordpressPost.edges[0].node
-    const imagesResolutions = post.featured_media.localFile.childImageSharp.resolutions
-    
-    return (
-        <Layout pageTitle={post.title} postExcerpt={post.excerpt}>
+  const post = data.allWordpressPost.edges[0].node
+  const imagesResolutions = post.featured_media.localFile.childImageSharp.resolutions
+
+  return (
+    <Layout pageTitle={post.title} postExcerpt={post.excerpt}>
+      <div className="post-page">
+        <h1>{post.title}</h1>
+        <div className="post-divider"></div>
+        <div className="post-data">
+          <div className="post-info">
+            <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          </div>
+          {imagesResolutions &&
             <div>
-                <h1>{post.title}</h1>
-                <div dangerouslySetInnerHTML={{ __html: post.content }} />
-                {imagesResolutions &&
-                    <div>
-                        <Img resolutions={imagesResolutions} />
-                    </div>
-                }
+              <Img className="post-image" resolutions={imagesResolutions} />
             </div>
-        </Layout>
-    )
+          }
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql`
