ed2ff2741116a6fd6f68850f3f0c39f2371239a2
elad <elad@twik.io>
2020-07-27 16:52:50 +0300

    Finished sidebar and footer design

--- src/components/layoutComps/Sidebar.js
+++ src/components/layoutComps/Sidebar.js
@@ -9,29 +9,32 @@ import { useSiteMetadata } from '../hooks'
 
 const navigationLinks = (pages) => {
     return (
-        <ul className="suka">
-            {pages.map((pageName, index) => {
-                const to = index === 0 ? "/" : "/" + pageName.toLowerCase() + "/";
-                return (<li className="mb-3 blat" key={index}>
-                    <Link
-                        to={to}
-                        className="align-bottom"
-                        activeClassName="current-page"
-                    >
-                        {pageName}
-                    </Link>
-                </li>)
-            })}
-        </ul>
+        <nav className="sidebar-navigation">
+            <ul className="suka">
+                {pages.map((pageName, index) => {
+                    const to = index === 0 ? "/" : "/" + pageName.toLowerCase() + "/";
+                    return (<li className="mb-3 blat" key={index}>
+                        <Link
+                            to={to}
+                            className="align-bottom"
+                            activeClassName="current-page"
+                            activeStyle={{ color: 'red' }}
+                        >
+                            {pageName}
+                        </Link>
+                    </li>)
+                })}
+            </ul>
+        </nav>
     );
 }
 
 const Sidebar = ({ pages }) => {
-  const { author } = useSiteMetadata()
+    const { author } = useSiteMetadata()
 
     return (
         <div className="sidebar-container">
-            <SiteCard siteData={{ siteTitle: config.siteTitle, description: config.siteDescription, photo: author.photo } }/>
+            <SiteCard siteData={{ siteTitle: config.siteTitle, description: config.siteDescription, photo: author.photo }} />
             <div className="sidebar-divider"></div>
             {navigationLinks(pages)}
         </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,13 +5,11 @@ import Layout from '../components/layout'
 export default function About() {
   return (
     <Layout headerText="About me" pageTitle="My site title">
-      <div style={{ color: `teal` }}>
         <div>
           <p>This is my apartments site</p>
           <p>These apartments are being occupied mostly by tourists and has a good atmosphere</p>
           <p>Your'e welcome to take a look at the available apartments. HF ;)</p>
         </div>
-      </div>
     </Layout>
   )
 }
