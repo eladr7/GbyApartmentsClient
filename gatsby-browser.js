967e15976d6c4f6c541e0f0fdf8833e2a399f1a2
elad <elad@twik.io>
2020-08-12 13:00:15 +0300

    Added graphql gen + my server's queries files + the apollo client with token handling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 // import "./src/styles/global.css"
+// or:
+// require('./src/styles/global.css')
 import React from "react"
 import UserContextProvider from "./src/stores/userContext"
 import FilterContextProvider from "./src/stores/filterContext"
@@ -6,23 +8,113 @@ import PaginationContextProvider from "./src/stores/paginationContext"
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-const client = new ApolloClient({
+import { getAccessToken, setAccessToken } from "./src/accessToken";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink, Observable } from "apollo-link";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import jwtDecode from "jwt-decode";
+
+
+const cache = new InMemoryCache({});
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable(observer => {
+      let handle;
+      Promise.resolve(operation)
+        .then(operation => {
+          const accessToken = getAccessToken();
+          if (accessToken) {
+            operation.setContext({
+              headers: {
+                authorization: `bearer ${accessToken}`
+              }
+            });
+          }
+        })
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer)
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
+const clientForMyServer = new ApolloClient({
+  link: ApolloLink.from([
+    new TokenRefreshLink({
+      accessTokenField: "accessToken",
+      isTokenValidOrUndefined: () => {
+        const token = getAccessToken();
+
+        if (!token) {
+          return true;
+        }
+
+        try {
+          const { exp } = jwtDecode(token);
+          if (Date.now() >= exp * 1000) {
+            return false;
+          } else {
+            return true;
+          }
+        } catch {
+          return false;
+        }
+      },
+      fetchAccessToken: () => {
+        return fetch("http://localhost:4000/refresh_token", {
+          method: "POST",
+          credentials: "include"
+        });
+      },
+      handleFetch: accessToken => {
+        setAccessToken(accessToken);
+      },
+      handleError: err => {
+        console.warn("Your refresh token is invalid. Try to relogin");
+        console.error(err);
+      }
+    }),
+    onError(({ graphQLErrors, networkError }) => {
+      console.log(graphQLErrors);
+      console.log(networkError);
+    }),
+    requestLink,
+    new HttpLink({
+      uri: "http://localhost:4001/graphql",
+      credentials: "include"
+    })
+  ]),
+  cache
+});
+
+
+const wpClient = new ApolloClient({
   uri: 'http://35.233.180.148/graphql'
 });
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <UserContextProvider>
-      <FilterContextProvider>
-        <ApolloProvider client={client}>
-          <PaginationContextProvider>
-            {element}
-          </PaginationContextProvider>
-        </ApolloProvider>
-      </FilterContextProvider>
-    </UserContextProvider>
+    <ApolloProvider client={clientForMyServer}>
+      <UserContextProvider>
+        <FilterContextProvider>
+          <ApolloProvider client={wpClient}>
+            <PaginationContextProvider>
+              {element}
+            </PaginationContextProvider>
+          </ApolloProvider>
+        </FilterContextProvider>
+      </UserContextProvider>
+    </ApolloProvider>
   )
 }
-
-// or:
-// require('./src/styles/global.css')
