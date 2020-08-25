9c15e64c3cdfecc08e04671a025925053bd1e616
elad <elad@twik.io>
2020-08-25 17:54:21 +0300

    Changed jid cookie path to '/' from '/refresh_token'

--- src/accessToken.ts
+++ src/accessToken.ts
@@ -7,7 +7,7 @@ const setCookie = (name, value, days) => {
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     expires = "; expires=" + date.toUTCString();
   }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/refresh_token";
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
 export const setAccessToken = (resData) => {
